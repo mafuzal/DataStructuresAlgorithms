6b0502840e4f9e49d13dc15f99773cf28563f392
mafuzalhoque <mafuzalhoque@gmail.com>
2022-02-24 22:05:38 -0500

    RemoveElements5

--- NT_RemoveElement.c
+++ NT_RemoveElement.c
@@ -1,18 +1,22 @@
 //Remove Elements 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 int removeElement(int* nums, int numsSize, int val);
 int main()
 {
     int nums[] = {2, 3, 9, 5, 2, 6};
-    removeElement(&nums, 7,2);
+    removeElement(nums, 7,2);
 
     return 0;
 }
 
 int removeElement(int* nums, int numsSize, int val)
 {
+    
     int temp[50] = {0};
+    int save[50] = {0};
     int count = 0;
     for (int i = 0; i< numsSize; i++)
     {
@@ -22,6 +26,23 @@ int removeElement(int* nums, int numsSize, int val)
         }
          
     }
+    bool checker = true;
+    for (int i = 0; i< count; i++)
+    {
+        for(int j = 0; j < count; j++)
+        {
+
+        }
+
+    }
+
+
+
+
+
+
+
+    
     
 
 }
