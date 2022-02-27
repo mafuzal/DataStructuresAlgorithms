5eb2a7cb8c35273a48a3b25e8e57b1c8f29a8fb6
mafuzalhoque <mafuzalhoque@gmail.com>
2022-02-26 22:58:32 -0500

    RemoveElements6

--- NT_RemoveElement.c
+++ NT_RemoveElement.c
@@ -14,14 +14,6 @@ int removeElement(int* nums, int numsSize, int val)
 {
     int temp[50] = {0};
     int count = 0;
-    for (int i = 0; i< numsSize; i++)
-    {
-        if (nums[i] == val)
-        {
-            temp[count++] = i;
-        }
-         
-    }
-    
+
 
 }
