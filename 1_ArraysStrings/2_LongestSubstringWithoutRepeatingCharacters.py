4c71a826f6992d02c53b6a7e5068c0a7a4e08174
mafuzalhoque <mafuzalhoque@gmail.com>
2023-01-03 23:37:22 -0500

    To Be Continued - Day 2

--- 1_ArraysStrings/2_LongestSubstringWithoutRepeatingCharacters.py
+++ 1_ArraysStrings/2_LongestSubstringWithoutRepeatingCharacters.py
@@ -2,3 +2,16 @@
 Arrays and Strings 
 Q2:Longest Substring Without Repeating Characters 
 """
+
+def lengthOfLongestSubstring(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    resultString = []
+    for i in range(len(s)):
+        if s[i] in resultString:
+            continue
+        else: 
+            resultString.append(s[i])
+    return len(s)    
